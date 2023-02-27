df3c9fd77222965c8f6b95624add7edc79389364
oleksandrbodnarchuk <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-02-27 11:52:38 +0100

    Added tests for TeskController updated code.Closes #81

--- src/test/java/com/javawwa25/app/springboot/controllers/TaskControllerTest.java
+++ src/test/java/com/javawwa25/app/springboot/controllers/TaskControllerTest.java
@@ -5,11 +5,15 @@ import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -24,12 +28,13 @@ import org.springframework.ui.Model;
 import com.javawwa25.app.springboot.project.dto.ProjectDto;
 import com.javawwa25.app.springboot.project.service.ProjectService;
 import com.javawwa25.app.springboot.task.controller.TaskController;
+import com.javawwa25.app.springboot.task.dto.TaskDto;
 import com.javawwa25.app.springboot.task.service.TaskService;
 import com.javawwa25.app.springboot.user.dto.UserDto;
 import com.javawwa25.app.springboot.user.service.UserService;
 
 @ExtendWith(MockitoExtension.class)
-class IssueControllerTest {
+class TaskControllerTest {
 	private final static String TASK_ENDPOINT = "/tasks/projects/{projectId}";
 	
 	@Mock
@@ -53,22 +58,62 @@ class IssueControllerTest {
 	}
 
 	@Test
+	void testProjectAllTasks() throws Exception {
+		String expected = "/task/task-list";
+		given(userService.getLoggedUserDto()).willReturn(UserDto.builder().build());
+		given(taskService.getAllTasksByProjectId(anyLong())).willReturn(Set.of());
+		String actual = underTest.projectAllTasks(1l, model);
+		verify(model, times(3)).addAttribute(any(), any());
+		verify(userService, times(1)).getLoggedUserDto();
+		verify(taskService, times(1)).getAllTasksByProjectId(anyLong());
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testProjectAllTasks_Request() throws Exception {
+		String expected = "/task/task-list";
+		mvc.perform(get(TASK_ENDPOINT, 1))
+		.andExpect(status().isOk())
+		.andExpect(view().name(expected));
+	}
+	
+	@Test
 	void testCreateTask() throws Exception {
 		String expected = "/task/new_task";
+		String actual = underTest.createTask("BUG", 1l, model);
+		verify(model, times(5)).addAttribute(any(), any());
+		verify(projectService, times(1)).getProjectNameById(1l);
+		verify(userService, times(1)).getLoggedUserDto();
+		verify(userService, times(1)).getSimpleDtos();
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testCreateTask_Request() throws Exception {
+		String expected = "/task/new_task";
 		given(userService.getLoggedUserDto()).willReturn(UserDto.builder().build());
-		mvc.perform(get(TASK_ENDPOINT + "/new", 1))
+		mvc.perform(get(TASK_ENDPOINT + "/new?type=BUG", 1))
 			.andExpect(status().isOk())
 			.andExpect(view().name(expected));
-		
-		String actual = underTest.createTask("BUG", 1l, model);
-		verify(model, times(3)).addAttribute(any(), any());
-		assertEquals(expected, actual);
 	}
 
 	@Test
 	void testSaveTask() {
-		String expected = "redirect:/users/{id}";
-		fail("Not yet implemented");
+		String expected = "redirect:/projects/" + 1 + "?success";
+		TaskDto dto = TaskDto.builder().userAssignedId(1l).build();
+		String actual = underTest.saveTask(1l, dto);
+		verify(taskService, times(1)).saveTask(dto);
+		verify(projectService, times(1)).assignProject(anyLong(), anyLong());
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testSaveTask_Request() throws Exception {
+		String expected = "redirect:/projects/" + 1 + "?success";
+		mvc.perform(post(TASK_ENDPOINT + "/new", 1)
+				.flashAttr("task", TaskDto.builder().build()))
+		.andExpect(status().is3xxRedirection())
+		.andExpect(view().name(expected));
 	}
 
 	@Test
@@ -96,21 +141,6 @@ class IssueControllerTest {
 		String expected = "redirect:/users/" + userId + "/projects/" + projectId + "/tasks";
 		;
 		fail("Not yet implemented");
-	}
-
-	@Test
-	void testProjectAllTasks() throws Exception {
-		String expected = "/task/task-list";
-		given(userService.getLoggedUserDto()).willReturn(UserDto.builder().build());
-		given(projectService.getProjectDtoById(anyLong())).willReturn(ProjectDto.builder().build());
-		mvc.perform(get(TASK_ENDPOINT, 1))
-		.andExpect(status().isOk())
-		.andExpect(view().name(expected));
-		
-		String actual = underTest.projectAllTasks(1l, model);
-		verify(model, times(2)).addAttribute(any(), any());
-		
-		assertEquals(expected, actual);
 	}
 
 }
