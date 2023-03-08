f33a73ec379ac46bcb06faa8dbbf1aeca2f296eb
oleksandr.bodnarchuk@outlook.com <oleksandrbodnarchuk@WAWPR211.presspublica.pl>
2023-03-08 10:13:06 +0100

    Refactored file code. Move to service layer streams.Closes #99

--- src/main/java/com/javawwa25/app/springboot/controller/UploadController.java
+++ src/main/java/com/javawwa25/app/springboot/controller/UploadController.java
@@ -1,17 +1,9 @@
 package com.javawwa25.app.springboot.controller;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.sql.SQLException;
 
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MimeTypeUtils;
-import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +12,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.javawwa25.app.springboot.file.FileData;
 import com.javawwa25.app.springboot.file.service.FileService;
-import com.javawwa25.app.springboot.user.service.UserService;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -32,41 +23,17 @@ import lombok.RequiredArgsConstructor;
 public class UploadController {
 
 	private final FileService fileService;
-	private final UserService userService;
 
 	@PostMapping("/upload")
 	public String uploadImage(@RequestParam("type") String type, MultipartFile file) throws IOException {
-		FileData fileData = fileService.save(file);
-		if (type.equals("avatar")) {
-			userService.updateAvatar(fileData);
-		}
+		fileService.save(type, file);
 		return "redirect:/user";
 	}
 
 	@GetMapping("/avatar")
 	public void getPhoto(HttpServletResponse resp) throws IOException {
 		FileData dbFile = fileService.getAvatar();
-		setResponseFile(resp, dbFile);
+		fileService.setResponseFile(resp, dbFile);
 	}
 
-	private void setResponseFile(HttpServletResponse resp, FileData dbFile) {
-		byte[] byteArray;
-		try (OutputStream os = resp.getOutputStream()) {
-			if (dbFile != null) {
-				byteArray = dbFile.getData().getBinaryStream().readAllBytes();
-				resp.setHeader("Content-Disposition", "attachment; filename=" + dbFile.getName());
-			} else {
-				InputStream is = new ClassPathResource("/static/images/avatar/default_avatar.jpg").getInputStream();
-				byteArray = is.readAllBytes();
-				is.close();
-				resp.setHeader("Content-Disposition", "attachment; filename=" + "default_avatar.jpg");
-			}
-			resp.setContentType(MimeTypeUtils.APPLICATION_OCTET_STREAM.getType());
-			resp.setContentLength(byteArray.length);
-			os.write(byteArray, 0, byteArray.length);
-		} catch (IOException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 }
--- src/main/java/com/javawwa25/app/springboot/file/service/FileService.java
+++ src/main/java/com/javawwa25/app/springboot/file/service/FileService.java
@@ -1,12 +1,18 @@
 package com.javawwa25.app.springboot.file.service;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.sql.Blob;
+import java.sql.SQLException;
 
 import org.hibernate.LobHelper;
 import org.hibernate.Session;
 import org.hibernate.engine.spi.SessionImplementor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.javawwa25.app.springboot.file.FileData;
@@ -14,6 +20,7 @@ import com.javawwa25.app.springboot.user.service.UserService;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
@@ -21,13 +28,16 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class FileService {
 
-	private final UserService userService; 
-	
+	private final UserService userService;
+
 	@PersistenceContext
 	private final EntityManager entityManager;
 
+	@Value("${avatar.default.location}")
+	private String defaultAvatarUri;
+	
 	@Transactional
-	public FileData save(MultipartFile file) {
+	public FileData save(String type, MultipartFile file) {
 		FileData fileData = new FileData();
 		try {
 			Session session = entityManager.unwrap(SessionImplementor.class);
@@ -42,16 +52,41 @@ public class FileService {
 			// TODO: personal exception
 			e.printStackTrace();
 		}
-
+		
+		if (type.equals("avatar")) {
+			userService.updateAvatar(fileData);
+		}
+		
 		return fileData;
 	}
 
 	public FileData getAvatar() {
 		FileData photo = userService.getLoggedUser().getAccount().getPhoto();
-		if(photo!=null) {
+		if (photo != null) {
 			return photo;
 		}
 		return null;
+	}
+
+	public void setResponseFile(HttpServletResponse resp, FileData dbFile) {
+		byte[] byteArray;
+		try (OutputStream os = resp.getOutputStream()) {
+			if (dbFile != null) {
+				byteArray = dbFile.getData().getBinaryStream().readAllBytes();
+				resp.setHeader("Content-Disposition", "attachment; filename=" + dbFile.getName());
+			} else {
+				InputStream is = new ClassPathResource(defaultAvatarUri).getInputStream();
+				byteArray = is.readAllBytes();
+				is.close();
+				resp.setHeader("Content-Disposition", "attachment; filename=" + "default_avatar.jpg");
+			}
+			resp.setContentType(MimeTypeUtils.APPLICATION_OCTET_STREAM.getType());
+			resp.setContentLength(byteArray.length);
+			os.write(byteArray, 0, byteArray.length);
+		} catch (IOException | SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
